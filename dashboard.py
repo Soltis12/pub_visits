049144c77e3a2aa6deaa14648168b285f2bffeda
Jono Pike <43720545+Soltis12@users.noreply.github.com>
2022-10-01 12:28:33 +0100

    Update dashboard.py
    Import of packages.

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,13 @@
+## Import packages
 import streamlit
+import pandas
+import snowflake.connector
+import requests
+from urllib.error import URLError
 
-streamlit.header("Holly's Pub Visits")
-streamlit.text("A map of happiness")
+## Opening text
+streamlit.title("🍺 Holly's Pub Visits 🍹")
+streamlit.header("Visualizing Happiness in 2022")
+streamlit.text("This tool is built to demonstrate usage of Snowflake, Python and APIs to import, clean, append to and visualize data")
+
+## Connect to Snowflake
