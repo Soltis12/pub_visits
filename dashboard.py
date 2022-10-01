667ed9fa512b9a9b6bd4472a491111548a531c30
Jono Pike <43720545+Soltis12@users.noreply.github.com>
2022-10-01 12:43:56 +0100

    Update dashboard.py
    Testing functionality to display data from Snowflake

--- dashboard.py
+++ dashboard.py
@@ -10,5 +10,15 @@ streamlit.title("🍺 Holly's Pub Visits 🍹")
 streamlit.header("Visualizing Happiness in 2022")
 streamlit.text("This tool is built to demonstrate usage of Snowflake, Python and APIs to import, clean, append to and visualize data")
 
-## Connect to Snowflake
-#
+# Function to display the table
+def get_snowflake_data():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * FROM PUBS_VISITED")
+        return my_cur.fetchall()
+
+## Load the data from Snowflake as a Pandas DataFrame
+if streamlit.button('Display Pub Visits'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_snowflake_data()
+    my_cnx.close
+    streamlit.dataframe(my_data_rows)
